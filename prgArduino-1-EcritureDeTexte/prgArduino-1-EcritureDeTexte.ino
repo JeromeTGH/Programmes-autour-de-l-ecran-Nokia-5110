c40117015ebf8cf20496910672e9aa227bc93503
JeromeTGH <jerome.tomski@gmx.fr>
2023-12-05 16:55:33 +0100

    Affichage d'une première ligne de texte à l'écran, pour test

--- prgArduino-1-EcritureDeTexte/prgArduino-1-EcritureDeTexte.ino
+++ prgArduino-1-EcritureDeTexte/prgArduino-1-EcritureDeTexte.ino
@@ -31,7 +31,7 @@
 #define brocheD8_arduino_vers_brocheRST_ecranNokia    8
 
 // Instanciation de cette librairie, de la sorte : "Adafruit_PCD8544(CLK,DIN,D/C,CE,RST);"
-Adafruit_PCD8544 display = Adafruit_PCD8544(brocheD4_arduino_vers_brocheCLK_ecranNokia, brocheD5_arduino_vers_brocheDIN_ecranNokia, brocheD6_arduino_vers_brocheDC_ecranNokia, brocheD7_arduino_vers_brocheCE_ecranNokia, brocheD8_arduino_vers_brocheRST_ecranNokia);
+Adafruit_PCD8544 ecranNokia5110 = Adafruit_PCD8544(brocheD4_arduino_vers_brocheCLK_ecranNokia, brocheD5_arduino_vers_brocheDIN_ecranNokia, brocheD6_arduino_vers_brocheDC_ecranNokia, brocheD7_arduino_vers_brocheCE_ecranNokia, brocheD8_arduino_vers_brocheRST_ecranNokia);
 
 
 
@@ -47,6 +47,34 @@ void setup() {
   Serial.println(F("================================================================="));
   Serial.println("");
 
+  // Allumage du rétroéclairage de l'écran
+  pinMode(brocheD3_arduino_vers_brocheLIGHT_ecranNokia, OUTPUT);
+  digitalWrite(brocheD3_arduino_vers_brocheLIGHT_ecranNokia, LOW);
+
+  // Écran Nokia 5110
+	ecranNokia5110.begin();             // Initialisation
+  ecranNokia5110.setContrast(56);     // Changement du contraste (entre 0 et 127) → à ajuster soi-même, au besoin
+  ecranNokia5110.setBias(4);          // Changement du biais (entre 0 et 7)       → à ajuster soi-même, au besoin
+  ecranNokia5110.clearDisplay();      // Effaçage de la mémoire tampon
+  delay(500);
+
+  // Suite du programme
+  afficherDuTexteAlEcran();
+
+}
+
+
+// ===========================
+// Afficher du texte à l'écran
+// ===========================
+void afficherDuTexteAlEcran() {
+  
+  ecranNokia5110.setTextSize(1);
+	ecranNokia5110.setTextColor(BLACK);
+	ecranNokia5110.setCursor(0,0);
+	ecranNokia5110.println("Salut a tous !");         // Attention, les accents peuvent entraîner des problèmes d'affichage, comme c'est le cas avec le "à" ici
+	ecranNokia5110.display();
+    
 }
 
 
